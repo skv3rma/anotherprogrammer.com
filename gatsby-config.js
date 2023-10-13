81c4422454f7b8fdf209895213d10db7dc93d7f3
skv3rma <skv3rma@gmail.com>
2023-10-13 06:02:17 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,39 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `notes`,
+        // Path to the directory
+        path: `${__dirname}/src/notes/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `projects`,
+        // Path to the directory
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        // Ignore files starting with a dot
+        ignore: [`**/\.*`],
+        // Use "mtime" and "inode" to fingerprint files (to check if file has changed)
+        fastHash: true,
+      },
+    },
+
+    "gatsby-transformer-remark",
+  ],
   siteMetadata: {
     title: "Web Warrior",
     description: "Web dev portfolio",
---
+++ src/projects/dojo-coffee-house.md
@@ -0,0 +1,26 @@
+---
+title: This Dojo Coffeee House
+stack: Html and CSS
+Slug: the-dojo-coffee-house
+---
+
+_Lorem ipsum dolor sit amet, consectetur adipiscing elit_,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+### Heading 1
+
+This is the first paragraph
+
+### Heading 2
+
+This is the second paragraph
+
+## Heading 2
+
+This is the second paragraph
+
+# Heading 2
+
+This is the second paragraph
